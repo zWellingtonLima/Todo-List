4219567ab6f61969e93ced011af7835ffa1eaa0d
zWellingtonLima <0wellingtonlima1@gmail.com>
2024-08-07 16:03:28 +0100

    fix: logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,11 @@
 import styles from "./Header.module.css";
+import logo from '../assets/logo.svg'
 
 export function Header() {
   return (
     <header className={styles.header}>
       <div>
-        <img src="/src/assets/logo.svg" alt="" />
+        <img src={logo} alt="" />
       </div>
     </header>
   );
