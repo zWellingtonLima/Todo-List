fbb77ef1d515c86fafc68d297210768194575f4b
zWellingtonLima <0wellingtonlima1@gmail.com>
2024-08-07 15:58:33 +0100

    style: changed import at header img logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,10 @@
 import styles from "./Header.module.css";
-import logo from "../assets/logo.svg";
 
 export function Header() {
   return (
     <header className={styles.header}>
       <div>
-        <img src={logo} alt="" />
+        <img src="/src/assets/logo" alt="" />
       </div>
     </header>
   );
